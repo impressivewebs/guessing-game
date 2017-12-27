e056e4498261daad22ef635e1d2f8b31976194a8
impressivewebs <impressive.webs@gmail.com>
2017-12-27 10:19:43 -0500

    Added files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Guessing Game
+
+## This readme file was built on:
+Wednesday, December 27, 2017
+
+## Number of lines of code in guessinggame.sh:
+26
---
+++ guessinggame.sh
@@ -0,0 +1,26 @@
+function getresponse {
+	echo "How many files are in the current directory?"
+	read response
+
+	numfiles=0
+	for files in $(ls)
+	do
+		let numfiles=$numfiles+1
+	done
+
+    if [[ $response -eq $numfiles ]]
+	then
+		echo "That's correct! Congratulations!"
+	else
+    	if [[ $response -lt $numfiles ]]
+		then
+			echo "That's too low! Try again."
+			getresponse
+		else
+			echo "That's too high! Try again."
+			getresponse
+		fi
+	fi
+}
+
+getresponse
